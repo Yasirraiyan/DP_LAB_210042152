d3ea181fc63a542f553d2761f2c0f1ec20d86bb2
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-06 21:07:43 +0600

    Create Bevarage.cs

---
+++ Coffee Shop Management System Lab-04/Bevarage.cs
@@ -0,0 +1,18 @@
+public class Bevarage
+{
+    public int price;
+    public string name;
+    public Bevarage(int price, string name)
+    {
+        this.price = price;
+        this.name = name;
+    }
+    public int getPrice()
+    {
+        return price;
+    }
+    public string getName()
+    {
+        return name;
+    }
+}
