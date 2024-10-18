3ad2051d2ca4c78b28a568d0e06817658a687c92
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-10-18 11:49:26 +0600

    Create ThirdPartyPostalMailService.cs

---
+++ Notification System Lab-06/ThirdPartyPostalMailService.cs
@@ -0,0 +1,8 @@
+public class ThirdPartyPostalMailService
+{
+    public void SendPostalMail(string reciepentAddress,string reciepentName)
+    {
+
+        Console.WriteLine($"Postal Mail sent to {recipientName} at {recipientAddress}: {message}");
+    }
+}
