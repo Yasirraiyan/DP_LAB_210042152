8c93aec6ab51a028d5749b0d5b407deab4f40533
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-11-22 12:31:05 +0600

    Create Next Prayer.cs

---
+++ Mosque System Lab-09 Madiator Pattern/Next Prayer.cs
@@ -0,0 +1,9 @@
+public class NextPrayer : IWidget
+{
+    private CurrentTime _currenttime;
+    public void SetCurrentTime(CurrentTime currenttime)
+    {
+
+    this._currenttime = currenttime; 
+    }
+}
