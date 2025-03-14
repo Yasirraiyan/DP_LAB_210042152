351467525af8b6c3c3325c9dd913e39a5321d78d
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-03-14 12:40:21 +0600

    Create File Persistance.cs

---
+++ Lab 12 Text Editor Issue Tracking/File Persistance.cs
@@ -0,0 +1,18 @@
+ public class FilePersistence
+ {
+
+     public static void SaveToFile(EditorMemento memento, string filePath)
+     {
+         string json = JsonSerializer.Serialize(memento);
+         File.WriteAllText(filePath, json);
+     }
+
+     public static EditorMemento LoadFromFile(string filePath)
+     {
+         if (!File.Exists(filePath))
+             throw new FileNotFoundException("File not found.");
+
+         string json = File.ReadAllText(filePath);
+         return JsonSerializer.Deserialize<EditorMemento>(json);
+     }
+ }
